116fc00f935119318dba1764898146ae011d0a6d
Veronica Bustabad <vbustabad@gmail.com>
2018-08-12 16:25:37 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -55,6 +55,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, note_contents: [])
   end
 end
