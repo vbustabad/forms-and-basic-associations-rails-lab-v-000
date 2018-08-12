83dca68226d243a7c9d0afd493a5da10366235f8
Veronica Bustabad <vbustabad@gmail.com>
2018-08-12 17:34:57 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,29 +45,6 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
-  end
-
-  def artist_name
-     self.artist ? self.artist.name : nil
-  end
-
-  def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
-  end
-
-  def genre_name
-     self.genre ? self.genre.name : nil
-  end
-
-  def note_contents=(contents)
-    contents.each do |content|
-      note = Note.find(content)
-      self.notes << note
-    end
-  end
-
   private
 
   def song_params
